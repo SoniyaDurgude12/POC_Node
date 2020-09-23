447d83443fad181eae8a088bc6d3502377b9f911
Aditi Varade <aditi_varade@persistent.co.in>
2020-09-23 13:21:40 +0530

    Commented createPooland added mysqlconnection code

--- Demo/config/database.js
+++ Demo/config/database.js
@@ -1,4 +1,4 @@
-const { createPool } = require("mysql");
+/* const { createPool } = require("mysql");
 
 const pool = createPool({
     host: process.env.DB_HOST,
@@ -7,6 +7,15 @@ const pool = createPool({
     password: process.env.DB_PASS,
     database: process.env.MYSQL_DB,
     connectionLimit: 5
+}); */
+
+var mysql = require('mysql');
+
+var pool = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "poc"
 });
 
 module.exports = pool;
