1e3855680040dcac69ed44c1046e65679ed8058f
soniyadurgude <soniyadurgude12@gmail.com>
2020-09-24 14:32:45 +0530

    Changed mysql database connectivity code

--- Demo/config/database.js
+++ Demo/config/database.js
@@ -1,4 +1,15 @@
-const { createPool } = require("mysql");
+const mysql =  require('mysql');
+
+const pool = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.MYSQL_DB
+});
+
+
+/* const { createPool } = require("mysql");
 
 const pool = createPool({
     host: process.env.DB_HOST,
@@ -7,6 +18,6 @@ const pool = createPool({
     password: process.env.DB_PASS,
     database: process.env.MYSQL_DB,
     connectionLimit: 5
-});
+}); */
 
 module.exports = pool;
